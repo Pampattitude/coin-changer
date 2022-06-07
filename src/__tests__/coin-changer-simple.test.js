ab7000328e6a7ade39ef148b23a041fc342d946b
Pampattitude <pampattitude@gmail.com>
2022-06-07 12:48:35 +0200

    added more tests

--- src/__tests__/coin-changer-simple.test.js
+++ src/__tests__/coin-changer-simple.test.js
@@ -27,6 +27,32 @@ describe("Coin changer", () => {
     });
   });
 
+  it("should return the correct change for input 10", () => {
+    const input = 10;
+
+    const result = optimalChange(input);
+
+    expect(result).toEqual({
+      2: 0,
+      5: 0,
+      10: 1,
+      rest: 0,
+    });
+  });
+
+  it("should return the correct change for input 11", () => {
+    const input = 11;
+
+    const result = optimalChange(input);
+
+    expect(result).toEqual({
+      2: 3,
+      5: 1,
+      10: 0,
+      rest: 0,
+    });
+  });
+
   it("should return the correct change with rest for input 3", () => {
     const input = 3;
 
